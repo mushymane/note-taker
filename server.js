e7f4ae96ca5f7062b4c59e56f759cb5fdd20cf62
Gabriel Lantin <mushymanee@gmail.com>
2021-10-20 22:45:50 -0700

    id

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
 //uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' use later
 
 const PORT = process.env.port || 3001;
