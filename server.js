0ff1077c82cc70eeef7668c88f2094de2cc08e02
Gabriel Lantin <mushymanee@gmail.com>
2021-10-20 23:50:17 -0700

    fixed port typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const notes = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 // const PORT = 3001;
 
 const app = express();
